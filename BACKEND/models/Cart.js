35d1a7c4281147f619eb1652d2c5df01d180e475
neydan-19 <bradvelarde@gmail.com>
2024-10-01 09:38:07 -0500

    Inicializacion del proyecto
    primer commit - iniciamos el entorno de trabajo para el ecommerce

---
+++ BACKEND/models/Cart.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/database')
+const User = require('./User')
+const Product = require('./Product')
+
+const Cart = sequelize.define('Cart', {
+    id: {type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
+    userId: {type: DataTypes.INTEGER, references: {model: User, key:'id'}},
+    productId: {type: DataTypes.INTEGER, references: {model: Product, key:'id'}},
+    quantity: {type: DataTypes.INTEGER, allowNull: false}
+})
+
+module.exports = Cart
