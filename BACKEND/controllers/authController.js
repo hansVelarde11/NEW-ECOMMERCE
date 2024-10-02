c700c169d4313d85ba6ad506850fbabfc9d7cd5c
Higor <higor13012005@gmail.com>
2024-10-01 19:37:58 -0500

    Controladores Auth y Product.
    Avance y desarrollo de los controladores

--- BACKEND/controllers/authController.js
+++ BACKEND/controllers/authController.js
@@ -1,8 +1,91 @@
+const Usuario = [
+    { id: 1, name: 'Higor Huaman', email: 'higor1302@gmail.com', password: 'Tutia_laLoca2', role: 'cliente' },
+    { id: 2, name: 'Carlos Ramirez', email: 'carlos.ramirez@gmail.com', password: 'C4rlos123!', role: 'admin' },
+    { id: 3, name: 'María Fernanda', email: 'mariaf@gmail.com', password: 'MFer_password99', role: 'cliente' },
+    { id: 4, name: 'Lucía Pérez', email: 'lucia.perez@hotmail.com', password: 'LuciaPerez2020', role: 'cliente' },
+    { id: 5, name: 'Juan Torres', email: 'juan.torres@yahoo.com', password: 'Ju4nTorres!', role: 'empleado' }
+];
+
+const bcrypt = require('bcrypt');
+
 //registerUser
+
+exports.registerUser = (req, res) =>{
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+
+    const newUser = {
+        id: Usuario.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        role: req.body.role || 'cliente'
+    }
+
+    Usuario.push(newUser);
+    res.status(201).json({
+        success: true,
+        data: newUser
+    });
+};
 
 //updateUser
 
+exports.updateUser = async (req, res) => {
+    const userId = parseInt(req.params.id)
+    const user = Usuario.find(usuario => usuario.id === userId)
+
+    if(!user){
+        return res.status(404).json({
+            success: false,
+            message: "Cliente no encontrado"
+        })
+    }
+   // Actualizar el nombre
+   if (req.body.name) {
+    user.name = req.body.name;
+}
+
+// Actualizar el email
+if (req.body.email) {
+    user.email = req.body.email;
+}
+
+
+if (req.body.password) {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    user.password = hashedPassword;
+}
+
+res.status(200).json({
+    success: true,
+    data: user
+});
+};
+
 //delete User
+
+exports.deleteUser = (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = Usuario.findIndex(usuario => usuario.id === userId);
+
+    if (userIndex === -1) {
+        return res.status(404).json({
+            success: false,
+            message: "Usuario no encontrado"
+        });
+    }
+
+   
+    Usuario.splice(userIndex, 1);
+
+    res.status(200).json({
+        success: true,
+        message: "Usuario eliminado exitosamente"
+    });
+};
+
 
 
 
--- BACKEND/controllers/productController.js
+++ BACKEND/controllers/productController.js
@@ -1,7 +1,99 @@
+const Producto = [
+        {
+            id: 1, name: 'Zapatillas Running Pro', description: 'Zapatillas ligeras y transpirables para corredores de larga distancia', price: 85.99, stock: 30
+        },
+        {
+            id: 2, name: 'Zapatillas Urban Style', description: 'Zapatillas de estilo urbano con suela antideslizante y diseño moderno', price: 69.99, stock: 45
+        },
+        {
+            id: 3, name: 'Zapatillas Deportivas All-Terrain', description: 'Zapatillas resistentes para todo tipo de terrenos, ideales para hiking y deportes al aire libre', price: 120.00, stock: 25
+        },
+        {
+            id: 4, name: 'Zapatillas Casual Comfort', description: 'Zapatillas casuales con plantilla de espuma viscoelástica para mayor comodidad', price: 55.00, stock: 60
+        },
+        {
+            id: 5, name: 'Zapatillas High Performance Basketball', description: 'Zapatillas de baloncesto de alto rendimiento con soporte extra para los tobillos', price: 140.00, stock: 20
+        }
+    ]  
+
+
 //GetProducts
+
+exports.GetProducts = (req,res) =>{
+    res.status(200).json({
+        success: true,
+        data: Producto
+    })
+}
+
 
 //CreateProduct
 
+exports.CreateProduct = (req,res) =>{
+    const newProduct = {
+        id: Producto.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        stock: req.body.stock
+    };
+    Producto.push(newProduct);
+    res.status(201).json({
+        success: true,
+        data: newProduct
+    });
+};
+
 //UpdateProduct
 
+exports.updateProduct = (req, res) => {
+    const productId = parseInt(req.params.id)
+    const product = Producto.find(producto => producto.id === productId)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        })
+    }
+
+    if (req.body.name) {
+        product.name = req.body.name
+    }
+
+    if (req.body.description){
+        product.description = req.body.description
+    }
+
+    if (req.body.price){
+        product.price = req.body.price
+    }
+
+    res.status(200).json({
+        success: true,
+        data: product
+    })
+
+}
+
 //DeleteProduct
+
+exports.deleteProduct = (req, res) => {
+    const productId = parseInt(req.params.id);
+    const productIndex = Producto.findIndex(producto => producto.id === productId);
+
+    if (productIndex === -1) {
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        });
+    }
+
+    
+    Producto.splice(productIndex, 1);
+
+    res.status(200).json({
+        success: true,
+        message: "Producto eliminado exitosamente"
+    });
+};
